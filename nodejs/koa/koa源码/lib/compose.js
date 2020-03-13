9950754c2adeaa372c3b7137a8b6bc6efaaf7759
Ryanx Chen <jsryanx@gmail.com>
2020-03-13 23:57:52 +0800

    compose

---
+++ nodejs/koa/koa源码/lib/compose.js
@@ -0,0 +1,4 @@
+// simpleKoaCompose
+const [fn1, fn2, fn3] = this.middleware;
+const fnMiddleware = function(context){
+  return Promise.resolve(fn1(context, function next(){        return Promise.resolve(          fn2(context, function next(){              return Promise.resolve(                  fn3(context, function next(){                    return Promise.resolve();                  })              )          })        )    })  );};fnMiddleware(ctx).then(handleResponse).catch(onerror);
