aab0b1c5af269e74df5e17f6b0fed04c5ee18e34
Ryanx Chen <jsryanx@gmail.com>
2020-04-01 23:47:15 +0800

    webpack

---
+++ 构建工具/webpack/demo/src/index.js
@@ -0,0 +1,2 @@
+const css = require('./css.css')
+console.log(123,css);
---
+++ 构建工具/webpack/demo/webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path')
+
+module.exports = {
+  mode: process.env.NODE_ENV,
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css/,
+        use: 'css-loader'
+      }
+    ]
+  }
+}
